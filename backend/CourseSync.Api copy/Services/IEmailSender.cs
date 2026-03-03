f8e51d6050f6999ac15630e8940c931ba6cb6cdc
ekhossejn <ekhossejn@gmail.com>
2026-03-04 02:07:17 +0300

    delete copy

--- backend/CourseSync.Api copy/Services/IEmailSender.cs
+++
@@ -1,6 +0,0 @@
-namespace CourseSync.Api.Infrastructure.Email;
-
-public interface IEmailSender
-{
-    Task SendAuthCodeAsync(string toEmail, string code, int ttlSeconds, CancellationToken ct = default);
-}
--- backend/CourseSync.Api copy/Services/VerifyResult.cs
+++
@@ -1,9 +0,0 @@
-namespace CourseSync.Api.Services;
-
-public enum VerifyResult
-{
-    Ok,
-    Invalid,
-    TooManyAttempts
-}
-
